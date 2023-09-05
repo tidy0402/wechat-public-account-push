eb3df547fb185be82b22183ea52a47c4ce0714e6
tidy0402 <142317358+tidy0402@users.noreply.github.com>
2023-09-05 11:56:18 +0800

    index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -30,20 +30,20 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1997', date: '02-21',
+          type: '*生日', name: '宝贝', year: '1997', date: '01-15',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '老婆', year: '1997', date: '02-21',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2023', date: '06-28',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2023-07-28' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
