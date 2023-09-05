4927c6910ddc157406c0759fbdf7bd2b521916bf
tidy0402 <142317358+tidy0402@users.noreply.github.com>
2023-09-05 11:50:56 +0800

    index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,6 +6,7 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
+  USE_PASSAGE: 'push-plus',
 
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
@@ -14,14 +15,14 @@ const USER_CONFIG = {
   APP_SECRET: '',
 
   PROVINCE: '广东',
-  CITY: '惠州',
+  CITY: '深圳',
 
   USERS: [
     {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'feb97744b9bf4b8cad09c291042c2256',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
